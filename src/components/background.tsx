e18d6a417fb48711e69b4bf80eae290395a807a3
Saha <sahanaamreen567@gmail.com>
2025-08-05 11:10:25 +0000

    Implement an infinite, animated starfield or drifting space clouds as th

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -3,13 +3,9 @@
 export function CursorGlowBackground() {
   return (
     <div className="fixed inset-0 -z-10 overflow-hidden bg-background">
-      <div className="blob-container">
-        <div className="blob one"></div>
-        <div className="blob two"></div>
-        <div className="blob three"></div>
-        <div className="blob four"></div>
-        <div className="blob five"></div>
-      </div>
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
     </div>
   );
 }
